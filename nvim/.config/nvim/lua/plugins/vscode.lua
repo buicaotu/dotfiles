2c00f52de743a42057143e0f44e81f1babed9043
Tu Bui <buicaotu@gmail.com>
2025-12-01 20:20:59 +1100

    .

--- nvim/.config/nvim/lua/plugins/vscode.lua
+++ nvim/.config/nvim/lua/plugins/vscode.lua
@@ -1,32 +1,18 @@
+local diff_red_light = '#911313'
+
 return {
   'Mofiqul/vscode.nvim',
   opts = {
-    -- Alternatively set style in setup
-    -- style = 'light'
-    -- Enable transparent background
     transparent = true,
-    -- Enable italic comment
     italic_comments = true,
-    -- Disable nvim-tree background color
     disable_nvimtree_bg = true,
-    -- Override colors (see ./lua/vscode/colors.lua)
     color_overrides = {
-      -- vscLineNumber = '#FFFFFF',
-      -- original
-      -- vscDiffRedDark = '#4B1818',
-      -- vscDiffRedLight = '#6F1313',
-      -- vscDiffRedLightLight = '#FB0101',
-      -- lighter light
-      -- vscDiffRedLight = "#8C0116",
-      vscDiffRedLightLight = '#911313',
+      vscDiffRedLightLight = diff_red_light,
       vscDiffGreenDark = "#006222",
       vscDiffGreenLight = "#033014",
     },
-    -- -- Override highlight groups (see ./lua/vscode/theme.lua)
     group_overrides = {
-        -- this supports the same val table as vim.api.nvim_set_hl
-        -- use colors from this colorscheme by requiring vscode.colors!
-        DiffText = { fg='NONE', bg='#911313', },
+        DiffText = { fg='NONE', bg=diff_red_light, },
     }
   },
   init = function()
