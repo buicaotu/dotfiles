9908163eac034296100ba62ff6e5868a7ba87a29
Tu Bui <buicaotu@gmail.com>
2025-12-01 20:21:20 +1100

    .

--- nvim/.config/nvim/lua/plugins/vscode.lua
+++ nvim/.config/nvim/lua/plugins/vscode.lua
@@ -1,4 +1,4 @@
-local diff_red_light = '#911313'
+local diff_red_light_light = '#911313'
 
 return {
   'Mofiqul/vscode.nvim',
@@ -7,12 +7,12 @@ return {
     italic_comments = true,
     disable_nvimtree_bg = true,
     color_overrides = {
-      vscDiffRedLightLight = diff_red_light,
+      vscDiffRedLightLight = diff_red_light_light,
       vscDiffGreenDark = "#006222",
       vscDiffGreenLight = "#033014",
     },
     group_overrides = {
-        DiffText = { fg='NONE', bg=diff_red_light, },
+        DiffText = { fg='NONE', bg=diff_red_light_light, },
     }
   },
   init = function()
