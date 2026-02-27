eec8f57105a6bf51570fcb6cec69a970bf582fb9
Tu Bui <buicaotu@gmail.com>
2026-02-27 19:28:52 +0700

    keymaps

--- nvim/.config/nvim/lua/personal/dap/init.lua
+++ nvim/.config/nvim/lua/personal/dap/init.lua
@@ -14,15 +14,17 @@ wk.add({
   { "<F9>", dap.step_over, desc = "Step over", mode = "n" },
   { "<F10>", dap.step_into, desc = "Step into", mode = "n" },
   { "<F7>", dap.step_out, desc = "Step out", mode = "n" },
-  { "<leader><leader>bc", dap.clear_breakpoints, desc = "Clear all breakpoints", mode = "n" },
-  { "<leader><leader>bb", dap.toggle_breakpoint, desc = "Toggle breakpoint", mode = "n" },
+  { "<F5>", dap.toggle_breakpoint, desc = "Toggle breakpoint", mode = "n" },
+  { "<leader>B",  group = "Breakpoints" },
+  { "<leader>Bb", dap.toggle_breakpoint, desc = "Toggle breakpoint", mode = "n" },
+  { "<leader>Bx", dap.clear_breakpoints, desc = "Clear all breakpoints", mode = "n" },
   {
-    "<leader><leader>bB",
+    "<leader>BB",
     function()
       dap.set_breakpoint(vim.fn.input('Breakpoint condition: '))
     end,
     desc = "Conditional breakpoint",
     mode = "n",
   },
-  { "<leader>br", dap.run_to_cursor, desc = "Run to cursor", mode = "n" },
+  { "<leader>Br", dap.run_to_cursor, desc = "Run to cursor", mode = "n" },
 })
