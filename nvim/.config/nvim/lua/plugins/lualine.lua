1ba066649526ad32c35236525b1ccda9549cf8a8
Tu Bui <buicaotu@gmail.com>
2026-04-03 13:41:13 +1100

    lualine tmux

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,28 +1,54 @@
 
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = {
-    options = { 
-      theme = 'auto',
-      section_separators = '',
-      component_separators = ''
-    },
-    sections = {
-      lualine_a = {'mode'},
-      lualine_b = {'branch', 'diff', 'diagnostics'},
-      lualine_c = {},
-      lualine_x = {'encoding', 'fileformat', 'filetype'},
-      lualine_y = {'progress'},
-      lualine_z = {'location'}
-    },
-    tabline = {
-      lualine_a = {'buffers'},
-      lualine_b = {},
-      lualine_c = {},
-      lualine_x = {},
-      lualine_y = {},
-      lualine_z = {'tabs'}
-    },
-  }
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+    'christopher-francisco/tmux-status.nvim',
+  },
+  config = function()
+    local tmux = require('tmux-status')
+    tmux.setup({
+      colors = {
+        window_active = '#d4d4d4',
+        window_inactive = '#858585',
+        window_inactive_recent = '#858585',
+        session = '#007acc',
+        datetime = '#858585',
+      },
+    })
+
+    require('lualine').setup({
+      options = {
+        theme = 'auto',
+        section_separators = '',
+        component_separators = ''
+      },
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {
+          {
+            'filename',
+            path = 1,
+            shorting_target = 40,
+          },
+        },
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {
+          'location',
+          { tmux.tmux_session, cond = tmux.show },
+          { tmux.tmux_windows, cond = tmux.show },
+        }
+      },
+      tabline = {
+        lualine_a = {'buffers'},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {'tabs'}
+      },
+    })
+  end,
 }
